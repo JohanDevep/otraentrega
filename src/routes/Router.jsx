a6918583d45d984368d6da588640f0eb4ab35130
caarlosdamian <dapecarlos@gmail.com>
2023-08-25 09:24:58 -0600

    fix(style): added some styling

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,21 +1,20 @@
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
 } from 'react-router-dom';
-import { Contacto, Intructores, Cursos } from '../Componentes/pages/index';
-import App from '../App';
-import Nabvar from '../Componentes/Navbar/Navbar'
+import { Contacto, Intructores, Cursos, Home } from '../pages/index';
+import { Root } from '../Componentes/Root/Root';
 
 export const router = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-            <Route element={<Nabvar />} path="/">
-                <Route element={<App />} index />
-                <Route element={<Contacto />} path="/contactos" />
-                <Route element={<Intructores />} path="/instructores" />
-                <Route element={<Cursos />} path="/cursos" />
-            </Route>
-        </>
-    )
+  createRoutesFromElements(
+    <>
+      <Route element={<Root />} path="/">
+        <Route element={<Home />} index />
+        <Route element={<Contacto />} path="/contactos" />
+        <Route element={<Intructores />} path="/instructores" />
+        <Route element={<Cursos />} path="/cursos" />
+      </Route>
+    </>
+  )
 );
